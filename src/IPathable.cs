b81208d4133cffd2fb7deafef588f1e21d7df0e3
Max Williams <max@welcometomonday.com>
2020-08-23 00:51:22 -0700

    Added new LocalCoordinate property to IPathable to make estimating distances via manhattan method easier

--- src/IPathable.cs
+++ src/IPathable.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 
+using Microsoft.Xna.Framework;
+
 namespace Ladybug.Pathfinding
 {
 	public interface IPathable
 	{
 		List<IPathable> GetNeighbors();
+		Vector2 LocalCoordinates {get;}
 		int GetDistanceEstimate(IPathable otherObject);
 	}
 }
